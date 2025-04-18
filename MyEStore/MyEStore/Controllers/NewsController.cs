ee6f8c154781c525b7bd8058dd21e573cbb7581d
P1emon <khang113324@gmail.com>
2025-04-18 21:51:48 +0700

    Sửa giao diện + thêm chức năng news (Tin tức)

---
+++ MyEStore/MyEStore/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEStore.Servicess;
+
+namespace MyEStore.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly RssFeedService _rssFeedService;
+
+        public NewsController()
+        {
+            _rssFeedService = new RssFeedService();
+        }
+
+        public async Task<IActionResult> Index(string category = null)
+        {
+            var allNews = await _rssFeedService.GetAllNewsItemsAsync();
+
+            ViewBag.CurrentCategory = category ?? "all";
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                allNews = allNews.Where(n => n.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            return View(allNews);
+        }
+
+    }
+}
