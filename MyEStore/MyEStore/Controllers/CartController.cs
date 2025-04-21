f3661c29aea9182e05bc1c2f4f2a3a39e3b63185
P1emon <khang113324@gmail.com>
2025-04-21 22:12:27 +0700

    Chỉnh lại giỏ hàng để nhận tiền giảm giá chứ không phải tiền gốc

--- MyEStore/MyEStore/Controllers/CartController.cs
+++ MyEStore/MyEStore/Controllers/CartController.cs
@@ -5,33 +5,33 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-	public class CartController : Controller
-	{
-		const string CART_KEY = "MY_CART";
-		private readonly MyeStoreContext _ctx;
+    public class CartController : Controller
+    {
+        const string CART_KEY = "MY_CART";
+        private readonly MyeStoreContext _ctx;
 
-		public List<CartItem> CartItems
-		{
-			get
-			{
-				var carts = HttpContext.Session.Get<List<CartItem>>(CART_KEY);
-				if (carts == null)
-				{
-					carts = new List<CartItem>();
-				}
-				return carts;
-			}
-		}
+        public List<CartItem> CartItems
+        {
+            get
+            {
+                var carts = HttpContext.Session.Get<List<CartItem>>(CART_KEY);
+                if (carts == null)
+                {
+                    carts = new List<CartItem>();
+                }
+                return carts;
+            }
+        }
 
-		public CartController(MyeStoreContext ctx)
-		{
-			_ctx = ctx;
-		}
+        public CartController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public IActionResult Index()
-		{
-			return View(CartItems);
-		}
+        public IActionResult Index()
+        {
+            return View(CartItems);
+        }
 
         [HttpGet]
         public IActionResult GetCartCount()
@@ -39,48 +39,50 @@ namespace MyEStore.Controllers
             var cartCount = CartItems.Sum(item => item.SoLuong);
             return Json(cartCount);
         }
-        public IActionResult AddToCart(int id, int qty = 1)
-		{
-			var cart = CartItems;
-			var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
-			if (cartItem != null)
-			{
-				cartItem.SoLuong += qty;
-			}
-			else
-			{
-				var hangHoa = _ctx.HangHoas.SingleOrDefault(h => h.MaHh == id);
-				if (hangHoa == null)
-				{
-					//không có trong database
-					TempData["ThongBao"] = $"Không tìm thấy hàng hóa có mã {id}";
-					return RedirectToAction("Index", "Products");
-				}
-				cartItem = new CartItem
-				{
-					MaHh = id,
-					SoLuong = qty,
-					TenHh = hangHoa.TenHh,
-					Hinh = hangHoa.Hinh,
-					DonGia = hangHoa.DonGia ?? 0
-				};
-				cart.Add(cartItem);
-			}
-			HttpContext.Session.Set(CART_KEY, cart);
-			return RedirectToAction("Index");
-		}
 
-		public IActionResult RemoveCartItem(int id)
-		{
+        public IActionResult AddToCart(int id, int qty = 1)
+        {
             var cart = CartItems;
             var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
             if (cartItem != null)
             {
-				cart.Remove(cartItem);
-				HttpContext.Session.Set(CART_KEY, cart);
+                cartItem.SoLuong += qty;
             }
-			return RedirectToAction("Index");
+            else
+            {
+                var hangHoa = _ctx.HangHoas.SingleOrDefault(h => h.MaHh == id);
+                if (hangHoa == null)
+                {
+                    TempData["ThongBao"] = $"Không tìm thấy hàng hóa có mã {id}";
+                    return RedirectToAction("Index", "Products");
+                }
+                cartItem = new CartItem
+                {
+                    MaHh = id,
+                    SoLuong = qty,
+                    TenHh = hangHoa.TenHh,
+                    Hinh = hangHoa.Hinh,
+                    DonGia = hangHoa.DonGia ?? 0,
+                    GiamGia = hangHoa.GiamGia // Store the discount
+                };
+                cart.Add(cartItem);
+            }
+            HttpContext.Session.Set(CART_KEY, cart);
+            return RedirectToAction("Index");
         }
+
+        public IActionResult RemoveCartItem(int id)
+        {
+            var cart = CartItems;
+            var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
+            if (cartItem != null)
+            {
+                cart.Remove(cartItem);
+                HttpContext.Session.Set(CART_KEY, cart);
+            }
+            return RedirectToAction("Index");
+        }
+
         [HttpPost]
         public IActionResult UpdateQuantity([FromBody] UpdateQuantityRequest request)
         {
@@ -93,8 +95,8 @@ namespace MyEStore.Controllers
                 HttpContext.Session.Set(CART_KEY, cart);
             }
 
-            var itemTotalPrice = cartItem?.ThanhTien ?? 0;
-            var cartTotal = cart.Sum(item => item.ThanhTien);
+            var itemTotalPrice = cartItem?.ThanhTien ?? 0; // Uses discounted price
+            var cartTotal = cart.Sum(item => item.ThanhTien); // Uses discounted price
 
             return Json(new
             {
@@ -108,11 +110,11 @@ namespace MyEStore.Controllers
             public int Id { get; set; }
             public int Qty { get; set; }
         }
-		[Authorize]
-        public IActionResult PaymentCallBack()
-		{
-			return View();
-		}
 
+        [Authorize]
+        public IActionResult PaymentCallBack()
+        {
+            return View();
+        }
     }
 }
