7b4db8a7ba194910b16ec40106a0746f599ec784
Pkmax-bit <2100010779@nttu.edu.vn>
2025-04-26 14:53:39 +0700

    Hiện view cơ bản ở admin

---
+++ MyEStore/MyEStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyEStore.Entities;
+
+namespace MyEStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly MyeStoreContext _context;
+
+        public ProductController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var myeStoreContext = _context.HangHoas.Include(h => h.MaLoaiNavigation).Include(h => h.MaNccNavigation);
+            return View(await myeStoreContext.ToListAsync());
+        }
+
+        // GET: Admin/Product/Create
+        public IActionResult Create()
+        {
+            ViewBag.MaLoai = new SelectList(_context.Loais, "MaLoai", "TenLoai");
+            ViewBag.MaNcc = new SelectList(_context.NhaCungCaps, "MaNcc", "TenNcc");
+            return View();
+        }
+
+        // POST: Admin/Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TenHh,TenAlias,MaLoai,MoTaDonVi,DonGia,Hinh,NgaySx,GiamGia,SoLanXem,MoTa,MaNcc")] HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hangHoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.MaLoai = new SelectList(_context.Loais, "MaLoai", "TenLoai", hangHoa.MaLoai);
+            ViewBag.MaNcc = new SelectList(_context.NhaCungCaps, "MaNcc", "TenNcc", hangHoa.MaNcc);
+            return View(hangHoa);
+        }
+
+        // GET: Admin/Product/CreateDetail
+        public IActionResult CreateDetail()
+        {
+            ViewBag.MaHh = new SelectList(_context.HangHoas, "MaHh", "TenHh");
+            return View();
+        }
+
+        // POST: Admin/Product/CreateDetail
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateDetail([Bind("MaHh,CongDung,DoiTuongSuDung,QuyCachDongGoi,ThanhPhan,CongNgheDacBiet,LoiIchNoiBat,LuuY")] HangHoaChiTiet hangHoaChiTiet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hangHoaChiTiet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index)); // Hoặc một action khác phù hợp
+            }
+            ViewBag.MaHh = new SelectList(_context.HangHoas, "MaHh", "TenHh", hangHoaChiTiet.MaHh);
+            return View(hangHoaChiTiet);
+        }
+    }
+}
---
+++ MyEStore/MyEStore/Areas/Admin/Controllers/SupplierController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyEStore.Entities;
+using System.Threading.Tasks;
+
+namespace MyEStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SupplierController : Controller
+    {
+        private readonly MyeStoreContext _context;
+
+        public SupplierController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Supplier
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.NhaCungCaps.ToListAsync());
+        }
+
+        // GET: Admin/Supplier/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhaCungCap = await _context.NhaCungCaps
+                .FirstOrDefaultAsync(m => m.MaNcc == id);
+            if (nhaCungCap == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhaCungCap);
+        }
+        // POST: Admin/Supplier/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaNcc,TenCongTy,Logo,NguoiLienLac,Email,DienThoai,DiaChi,MoTa")] NhaCungCap nhaCungCap)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nhaCungCap);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhaCungCap);
+        }
+
+        // GET: Admin/Supplier/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhaCungCap = await _context.NhaCungCaps.FindAsync(id);
+            if (nhaCungCap == null)
+            {
+                return NotFound();
+            }
+            return View(nhaCungCap);
+        }
+
+        // POST: Admin/Supplier/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaNcc,TenCongTy,Logo,NguoiLienLac,Email,DienThoai,DiaChi,MoTa")] NhaCungCap nhaCungCap)
+        {
+            if (id != nhaCungCap.MaNcc)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nhaCungCap);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhaCungCapExists(nhaCungCap.MaNcc))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhaCungCap);
+        }
+
+        // GET: Admin/Supplier/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhaCungCap = await _context.NhaCungCaps
+                .FirstOrDefaultAsync(m => m.MaNcc == id);
+            if (nhaCungCap == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhaCungCap);
+        }
+
+        // POST: Admin/Supplier/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var nhaCungCap = await _context.NhaCungCaps.FindAsync(id);
+            _context.NhaCungCaps.Remove(nhaCungCap);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NhaCungCapExists(string id)
+        {
+            return _context.NhaCungCaps.Any(e => e.MaNcc == id);
+        }
+    }
+}
