69c090e0d5254b0f24f526522c2fdd44bcef2f75
Pkmax-bit <2100010779@nttu.edu.vn>
2025-05-03 15:23:28 +0700

    Thêm vào bảng thông báo

---
+++ MyEStore/MyEStore/Migrations/20250503081828_AddThongBaoToTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyEStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThongBaoToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.CreateTable(
+                name: "ThongBao",
+                columns: table => new
+                {
+                    MaTb = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TieuDe = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    NoiDung = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NgayTao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaKh = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MaMv = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MaSlider = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DaXem = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThongBao", x => x.MaTb);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ThongBaos",
+                columns: table => new
+                {
+                    MaTb = table.Column<int>(type: "int", nullable: false),
+                    TieuDe = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoiDung = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NgayTao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaKh = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaMv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaSlider = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DaXem = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThongBao");
+
+            migrationBuilder.DropTable(
+                name: "ThongBaos");
+
+
+        }
+    }
+}
