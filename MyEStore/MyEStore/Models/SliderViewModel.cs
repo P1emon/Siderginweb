152f2779eb10883f3540579ac114abc3cd284b3f
Pkmax-bit <2100010779@nttu.edu.vn>
2025-05-05 10:37:12 +0700

    Sửa lại slider model hình ảnh thong báo

---
+++ MyEStore/MyEStore/Models/SliderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MyEStore.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyEStore.Models
+{
+    public class SliderViewModel
+    {
+
+        public int MaSlider { get; set; }
+        public string TieuDe { get; set; }
+        public string MoTa { get; set; }
+        public string HinhAnh { get; set; }
+        public string LinkQuangCao { get; set; }
+        public DateTime NgayTao { get; set; } = DateTime.Now;
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+
+    }
+}
